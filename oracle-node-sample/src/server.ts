d955d9d2ee98e02060b89932b3a397c90119670f
Marcos Junior <juninhomazzero@gmail.com>
2024-10-30 23:25:08 -0300

    feat: add user registration endpoint and frontend integration

--- oracle-node-sample/src/server.ts
+++ oracle-node-sample/src/server.ts
@@ -1,9 +1,13 @@
 import express, { Request, Response, Router } from 'express';
+import cors from 'cors';
 import DataBase from './connection';
 
 const app = express();
 const port = 3000;
 const routes = Router()
+
+app.use(express.json());
+app.use(cors());
 
 routes.get('/', (req: Request, res: Response) => {
     res.status(200).send("Funcionando...");
@@ -30,6 +34,39 @@ routes.get('/test', async (req: Request, res: Response) => {
     }
   }
 });
+
+routes.post('/register', async (req: Request, res: Response) => {
+  console.log('Recebendo requisição no endpoint /register');
+
+  const { name, email, telefone, cpf, data_nasc, endereco, peso, altura } = req.body;
+  let connection;
+
+  console.log('Dados recebidos:', req.body);
+
+  try {
+    connection = await DataBase.connect();
+    console.log('Conexão com o banco de dados estabelecida');
+
+    const insertQuery = `
+      INSERT INTO alunos (name, email, telefone, cpf, data_nasc, endereco, peso, altura)
+      VALUES (:name, :email, :telefone, :cpf, TO_DATE(:data_nasc, 'YYYY-MM-DD'), :endereco, :peso, :altura)
+    `;
+
+    await connection.execute(insertQuery, [name, email, telefone, cpf, data_nasc, endereco, peso, altura]);
+    await connection.commit();
+
+    res.status(201).json({ message: 'Cadastro realizado com sucesso!' });
+  } catch (err) {
+    console.error('Erro ao cadastrar o usuário:', err);
+    res.status(500).json({ message: 'Erro ao cadastrar o usuário.' }); // Resposta em JSON
+  } finally {
+    if (connection) {
+      await DataBase.close(connection);
+      console.log('Conexão com o banco de dados fechada');
+    }
+  }
+});
+
 
 app.use(routes);
 
