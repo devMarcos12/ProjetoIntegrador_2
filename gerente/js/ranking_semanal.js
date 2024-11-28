1311f359a4be00f17fafecbcdd919550b714a094
Bianca-Silvaaa28 <silvabianca147@gmsil.com>
2024-11-28 20:43:44 -0300

    backend relatório gerente semanal

--- gerente/js/ranking_semanal.js
+++ gerente/js/ranking_semanal.js
@@ -1,7 +1,40 @@
+async function fetchRankingSemanal() {
+    try {
+        const response = await fetch('http://localhost:3000/ranking-semanal');
+        if (!response.ok) {
+            throw new Error('Erro ao buscar o ranking semanal');
+        }
+
+        const data = await response.json();
+        console.log(data)
+        const rankingTableBody = document.getElementById('rankingTableBody');
+        rankingTableBody.innerHTML = ''; // Limpa a tabela antes de inserir novos dados
+
+        data.forEach(row => {
+            const tr = document.createElement('tr');
+            tr.innerHTML = `
+                <td>${row.CLASSIFICACAO}</td>
+                <td>${row.ALUNO_CPF}</td>
+                <td>${row.ALUNO_NOME}</td>
+                <td>${row.TOTAL_HORAS_TREINADAS}</td>
+            `;
+            rankingTableBody.appendChild(tr);
+        });
+
+        updateStudentCount(); // Atualiza a contagem após o fetch
+        sortTableByTotal();   // Ordena após preencher
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+
+
 function updateStudentCount() {
-    var studentCount = document.getElementById('studentTableBody').getElementsByTagName('tr').length;
+    const studentCount = document.getElementById('rankingTableBody').getElementsByTagName('tr').length;
     document.getElementById('quantity').textContent = studentCount;
 }
+
 
 function sortTableByTotal() {
     var tableBody = document.getElementById('studentTableBody');
@@ -24,5 +57,6 @@ function sortTableByTotal() {
 
 window.onload = function () {
     updateStudentCount();
+    fetchRankingSemanal();
     sortTableByTotal(); // Ordena a tabela pela coluna TOTAL ao carregar a página
 };
